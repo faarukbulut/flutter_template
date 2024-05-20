fa3d3c77b0f594ca590be052140aaa6c30834fe9
Faruk Bulut <72122797+faarukbulut@users.noreply.github.com>
2024-05-20 22:16:44 +0300

    .

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
 class AppConstants{
-  static String apiBaseUrl = "API_URL";
+  static const String apiBaseUrl = "API_URL";
 
-  static String androidVersion = "1.0.0";
-  static String iOSVersion = "1.0.0";
+  static const String androidVersion = "1.0.0";
+  static const String iOSVersion = "1.0.0";
 
-  static Color kPrimary = const Color.fromARGB(255, 51, 77, 195); // Ana Renk
+  static const Color kPrimary = Color.fromARGB(255, 51, 77, 195); // Ana Renk
 
 }
